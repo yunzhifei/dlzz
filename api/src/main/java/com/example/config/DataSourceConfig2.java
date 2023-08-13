43bd9f5595c17305083bee75b9d21130559b166e
yunzhifei_chd@139.com <yunzhifei_chd@139.com>
2023-08-13 11:38:44 +0800

    打印sql日志问题

--- api/src/main/java/com/example/config/DataSourceConfig2.java
+++ api/src/main/java/com/example/config/DataSourceConfig2.java
@@ -1,15 +1,15 @@
 package com.example.config;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import javax.sql.DataSource;
@@ -18,15 +18,32 @@ import javax.sql.DataSource;
 @MapperScan(basePackages = "com.example.dao.second", sqlSessionTemplateRef = "blogSqlSessionTemplate2")
 public class DataSourceConfig2 {
 
+    @Value("${spring.datasource.db2.jdbcUrl}")
+    private String url;
+
+    @Value("${spring.datasource.db1.username}")
+    private String name;
+
+    @Value("${spring.datasource.db1.password}")
+    private String password;
+
     @Bean(name = "blogDataSource2")
     @ConfigurationProperties(prefix = "spring.datasource.db2")
     public DataSource blogDataSource() {
-        return DataSourceBuilder.create().build();
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setUrl(url);
+        druidDataSource.setUsername(name);
+        druidDataSource.setPassword(password);
+        druidDataSource.setMaxActive(10);
+        druidDataSource.setMinIdle(5);
+        druidDataSource.setInitialSize(10);
+        return druidDataSource;
     }
 
     @Bean(name = "blogSqlSessionFactory2")
     public SqlSessionFactory blogSqlSessionFactory(@Qualifier("blogDataSource2") DataSource dataSource) throws Exception {
         SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+
         bean.setDataSource(dataSource);
         bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:second/*.xml"));
         return bean.getObject();
