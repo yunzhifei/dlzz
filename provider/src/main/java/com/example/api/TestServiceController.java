cdf39b0678626fcbadbbcb2712b993d2d5683090
yunzhifei_chd@139.com <yunzhifei_chd@139.com>
2023-09-11 15:25:09 +0800

    注释配置

--- provider/src/main/java/com/example/api/TestServiceController.java
+++ provider/src/main/java/com/example/api/TestServiceController.java
@@ -1,17 +1,25 @@
 package com.example.api;
 
-import com.example.vo.BaseVO;
-import com.example.vo.ReqVO;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.vo.BaseVO;
+import com.example.vo.ReqVO;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @Api("testService")
 public class TestServiceController {
     @GetMapping("/test/list")
     @ApiOperation("测试接口")
+    /***
+     *
+     * @author yunzhifei
+     * @date 2023/09/11 15:22
+     * @return com.example.vo.BaseVO<com.example.vo.ReqVO>
+     */
     public BaseVO<ReqVO> get() {
         BaseVO baseVO =  new BaseVO<>();
         ReqVO reqVO = new ReqVO();
